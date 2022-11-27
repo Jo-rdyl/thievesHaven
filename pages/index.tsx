010e82eff088c8cea30a0a12ee1d2298be50d8fc
Jahin Khan <jahin.khan693@gmail.com>
2022-11-27 14:43:36 +0000

    Did a box thingy (test)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/index.module.scss';
+import React, { useState } from 'react';
 
 
 const Page = () => {
@@ -8,17 +9,33 @@ const Page = () => {
       <Head>
         <title>thievesHaven</title>
       </Head>
-      <main className={styles.main}>
+      <>
         <h1>Hello World!</h1>
         <p className={styles.one}>Hello <span>World</span></p>
         <p className={styles.two}>Hello <span>World</span></p>
         <p className={styles.three}>Hello <span>World</span></p>
         <p className={styles.four}>Hello <span>World</span></p>
-      </main>
+        <Box />
+      </>
     </div>
   );
 };
 
+const Box = () => {
+  const [phase,setPhase] = useState(0);
+
+  return (
+    <>
+      <div style={{borderRadius: `${phase}%`}} className={styles.box} onMouseOver={() => {
+        if (phase == 50) {
+          setPhase(phase - 50)
+        } else {
+          setPhase(phase + 12.5)
+        }
+      }}></div>
+    </>
+  )
+}
 
 
 export default Page;
