05db095f5fdac077ab3c303b7264f62998c27630
Jo-rdyl <jordyluyinga@gmail.com>
2022-11-29 21:59:54 +0000

    Revert "added d3"
    This reverts commit 4fffdc4029e3948f8659e12c7d67030e62f7e0a5.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/index.module.scss';
 import React, { useState } from 'react';
-import * as d3 from 'd3';
 
 
 const Page = () => {
@@ -12,8 +11,11 @@ const Page = () => {
       </Head>
       <>
         <h1>Hello World!</h1>
+        <p className={styles.one}>Hello <span>World</span></p>
+        <p className={styles.two}>Hello <span>World</span></p>
+        <p className={styles.three}>Hello <span>World</span></p>
+        <p className={styles.four}>Hello <span>World</span></p>
         <Box />
-        <D3 />
       </>
     </div>
   );
@@ -21,27 +23,19 @@ const Page = () => {
 
 const Box = () => {
   const [phase,setPhase] = useState(0);
+
   return (
     <>
       <div style={{borderRadius: `${phase}%`}} className={styles.box} onMouseOver={() => {
         if (phase == 50) {
           setPhase(phase - 50)
         } else {
-          setPhase(phase + 1)
+          setPhase(phase + 12.5)
         }
       }}></div>
     </>
   )
 }
-
-const D3 = () => {
-  return (
-    <div id='container'></div>
-  );
-};
-
-
-d3.select('#container').append('p').text("This is written by d3");
 
 
 export default Page;
