75b5dd2f280caa2b488fd1585430792effbc5983
Jahin Khan <jahin.khan693@gmail.com>
2022-12-17 16:12:55 +0000

    Code cleanup & optimisation, minor style changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,7 @@ const Page = () => {
   );
 };
 
+
 const Header = () => {
   return (
     <header className={styles.Header}>
@@ -36,6 +37,7 @@ const Header = () => {
   );
 };
 
+
 const ArtStuff = () => {
   const [dimensions,setDimensions] = useState({
     height: 0,
@@ -54,66 +56,53 @@ const ArtStuff = () => {
     (dimensions.width % 45) / 2, (dimensions.height % 45) / 2
   ]
 
-  // Desktop: 4.5vw, 22*14= 308circles, 100vw/64vw, 1vw left
-  // Mobile: 12vw, 8*20= 160circles, 100vw/250vw, 4vw left
-
   const locations = []
-  for (let x = 22.5; x < dimensions.width; x += 45) {
-    for (let y = 22.5; y < dimensions.height; y += 45) {
+
+  for (let x = 0; x < dimensions.width - 45; x += 45) {
+    for (let y = 0; y < dimensions.height - 45; y += 45) {
       locations.push([
-        x - halfSpace[0],
-        y - halfSpace[1]
+        x + halfSpace[0],
+        y + halfSpace[1]
       ])
     }
   }
 
-  const smallPossibleLocations = [...locations];
-  const medPossibleLocations = [...locations];
-  const bigPossibleLocations = [...locations];
-  const smallLocations = [];
-  const medLocations = [];
-  const bigLocations = [];
-  const numberOfCircles = locations.length / 1.3;
+  const smallLocations = locations.map((location) => [location[0] + 15, location[1] + 15]);
+  const medLocations = locations.map((location) => [location[0] + 7.5, location[1] + 7.5]);
+  const bigLocations = [...locations];
+  const numberOfCircles = locations.length * 0.25;
   
   for (let i = locations.length - 1; i > numberOfCircles; i--) {
-    let s = Math.trunc(Math.random() * i);
-    let m = Math.trunc(Math.random() * i);
-    let b = Math.trunc(Math.random() * i);
-
-    smallLocations.push(smallPossibleLocations[s]);
-    medLocations.push(medPossibleLocations[m]);
-    bigLocations.push(bigPossibleLocations[b]);
-
-    smallPossibleLocations.splice(s,1);
-    medPossibleLocations.splice(m,1);
-    bigPossibleLocations.splice(b,1);
+    smallLocations.splice(Math.trunc(Math.random() * i),1);
+    medLocations.splice(Math.trunc(Math.random() * i),1);
+    bigLocations.splice(Math.trunc(Math.random() * i),1);
     }
 
-    const smallMapped = smallLocations.map((location) => {
-      return (
-        <div
-        className={styles.small}
-        style={{left: `${location[0] + 15}px`, top: `${location[1] + 15}px`}}
-        key={`s${location}`} />
-      )
-    });
-    const medMapped = medLocations.map((location) => {
-      return (
-        <div
-        className={styles.med}
-        style={{left: `${location[0] + 7.5}px`, top: `${location[1] + 7.5}px`}}
-        key={`m${location}`} />
-      )
-    });
-    const bigMapped = bigLocations.map((location) => {
-      return (
-        <div
-        className={styles.big}
-        style={{left: `${location[0]}px`, top: `${location[1]}px`}}
-        key={`b${location}`} />
-      )
-    });
 
+  const smallMapped = smallLocations.map((location) => {
+    return (
+      <div
+      className={styles.small}
+      style={{left: `${location[0]}px`, top: `${location[1]}px`}}
+      key={`s${location}`} />
+    )
+  });
+  const medMapped = medLocations.map((location) => {
+    return (
+      <div
+      className={styles.med}
+      style={{left: `${location[0]}px`, top: `${location[1]}px`}}
+      key={`m${location}`} />
+    )
+  });
+  const bigMapped = bigLocations.map((location) => {
+    return (
+      <div
+      className={styles.big}
+      style={{left: `${location[0]}px`, top: `${location[1]}px`}}
+      key={`b${location}`} />
+    )
+  });
 
   return (
     <div className={styles.ArtStuff} ref={ref}>
