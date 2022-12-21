d62d436b154a027ec78a3eeedc1fb130f0267650
Jo-rdyl <jordyluyinga@gmail.com>
2022-12-21 22:46:38 +0000

    not finished!

--- pages/weather.tsx
+++ pages/weather.tsx
@@ -1,25 +1,25 @@
-const fetcher = (...args) => fetch(...args).then(res => res.json())
 import useSWR, {SWRConfig} from 'swr';
 import { useState } from 'react';
-const apiKey = `51e69e6229683f0c12fea8d17f57063e`
+const fetcher = (url) => fetch(url).then(res => res.json())
 
 export default function App() {
 const [city,setCity] = useState("");
 
-    return(
+return(
         <>
         <h1>hello!</h1>
         <label>City: <input type="text" value={city} onChange={(e:any) => setCity(e.target.value)}/></label>
         <button onClick={getWeather}>Search</button>
+        <getWeather/>
         </>
     );
 };
 
-function getWeather(city:any) {
-    const { data, error, isLoading } = useSWR(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`, fetcher);
+function getWeather(city) {
+const { data, error} = useSWR(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=51e69e6229683f0c12fea8d17f57063e`, fetcher);
     if (error) {
-        return alert(`There's been an error {error}`)
+        return <h1>{data.error}</h1>
     }
-    return alert(data[0])
+    return <h2>{data[0]}</h2>
 
 }
