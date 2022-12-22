394d7be568724ba786379ca2bf02f63d1a20416f
Jo-rdyl <jordyluyinga@gmail.com>
2022-12-22 00:09:02 +0000

    making it netlify friendly

--- pages/weather.tsx
+++ pages/weather.tsx
@@ -9,17 +9,8 @@ return(
         <>
         <h1>hello!</h1>
         <label>City: <input type="text" value={city} onChange={(e:any) => setCity(e.target.value)}/></label>
-        <button onClick={getWeather}>Search</button>
-        <getWeather/>
+        <button>Search</button>
         </>
     );
 };
 
-function getWeather(city) {
-const { data, error} = useSWR(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=51e69e6229683f0c12fea8d17f57063e`, fetcher);
-    if (error) {
-        return <h1>{data.error}</h1>
-    }
-    return <h2>{data[0]}</h2>
-
-}
