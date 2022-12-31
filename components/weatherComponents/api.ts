a58074d7a84a71f177aed8b25b8e581a81521df9
Jo-rdyl <jordyluyinga@gmail.com>
2022-12-31 06:52:05 +0000

    working weather puller results print to console!

--- components/weatherComponents/api.ts
+++
@@ -1,9 +0,0 @@
-export const GeoAPIOptions = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'fe21157b25msh5ee270bee489cfap15338fjsna0ebab076bd8',
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-	}
-};
-
-export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities'
--- pages/weather.tsx
+++ pages/weather.tsx
@@ -1,3 +1,28 @@
-import Search from '../components/weatherComponents/search'
-const App = () => <Search />
-export default App;
+import fetchCityWeather from '../components/fetchCityWeather';
+
+export default function App() {
+  return (
+    <section style={{textAlign:"center"}}>
+      <h1>Weather Test App</h1>
+      <input type="text" placeholder="enter city name" id="cityName"/>
+      <input type="submit" onClick={handleClick}/>
+      <p id="Jordy">Written by Jordy</p>
+        <p id="OpenWeather">Powered by OpenWeather™</p>
+        <p id="status"></p>
+        <style jsx>{`
+        #Jordy{
+            font-family: Ubuntu, sans-serif;
+            text-align: center;
+            box-sizing: border-box;
+            margin-top: 8px;
+            margin-bottom: 8px;
+        }`}</style>
+      </section>
+  );
+}
+const handleClick = () => {
+  const GetCityName = document.querySelector("#cityName")
+  const CityName = GetCityName.value;
+  fetchCityWeather(CityName)
+
+}
