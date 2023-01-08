d200be1fe2aa79fe2231a92ab5ae9cb1d869aba2
Jo-rdyl <jordyluyinga@gmail.com>
2023-01-08 15:06:15 +0000

    This should fix Netlify now

--- components/weather.tsx
+++ components/weather.tsx
@@ -2,8 +2,8 @@ import { useState } from 'react';
 
 function Weather() {
   const [weatherData,SetWeatherData] = useState(null)
-  const [city,setCity] = useState("")
-  const [searchData,setSearchData] = useState(null)
+  const [city,setCity] = useState<string>("")
+  const [searchData,setSearchData] = useState<any>(null)
   const fetchCityWeather = async (event: any) => {
 
     event.preventDefault();  //Stop the form from submitting and refreshing the page
@@ -12,10 +12,10 @@ function Weather() {
 
     try {
       const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=\
-      ${theCityName}&units=metric&appid=51e69e6229683f0c12fea8d17f57063e`);//the url to fetch the weather infomation
+      ${theCityName}&units=metric&appid=51e69e6229683f0c12fea8d17f57063e`);//the url to fetch the weather information
       const data = await response.json();//turns the JSON into a ECMAScript object (fancy way of saying JS)
       SetWeatherData(data);//Sets the  weatherDta state from null to our new JS object!
-      console.log(JSON.stringify(data,null,2));//Garnage shit that i'll remove
+      console.log(JSON.stringify(data,null,2));//Garbage shit that I'll remove
     }
     catch (error) {
       console.log("Error")//if it fails to get the weather data e.g. bad spelling, an error is written to the logs
@@ -29,12 +29,12 @@ let SearchBar = async (e:any) =>{ //e is short for event I just like using it
     headers: {
       'X-RapidAPI-Key': 'fe21157b25msh5ee270bee489cfap15338fjsna0ebab076bd8',
       'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-    } // above are the GeoDB configurations so it actually works (yes it's necessary)
+    } // above are the GeoDB configurations, so it actually works (yes it's necessary)
   };
   try{
   let response = await fetch(`https://wft-geo-db.p.rapidapi.com/v1/geo/cities?&offset=2&namePrefix=${city}`,options);
   const data = await response.json();//turns the city names into JS objects
-  const actualData = data.data;//thhis helps with mapping the cities later on
+  const actualData = data.data;//this helps with mapping the cities later on
   console.log(data)
   console.log(data["data"]["name"])
   console.log(actualData[1])
@@ -48,7 +48,7 @@ let SearchBar = async (e:any) =>{ //e is short for event I just like using it
     <>
     <section>
       <h2>Weather</h2>
-      <form onSubmit={fetchCityWeather}> {/*pretty self explanatory */}
+      <form onSubmit={fetchCityWeather}> {/*pretty self-explanatory */}
       <input type="text" required placeholder="Enter the city's name" name="city" value={city} onChange={SearchBar}/>
       <button type='submit'>Submit</button>
       </form>
